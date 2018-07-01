b2afa861a82f4c2a3572f37c588e752e41b140a6
Rushikesh Jogdand <rushikeshjogdand1@gmail.com>
2018-07-01 19:35:34 +0530

    added cloud functions to update users, material and course data (#16)

--- cloudFunctions/functions/index.js
+++ cloudFunctions/functions/index.js
@@ -1,8 +1,66 @@
 const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+let db;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+db = admin.database();
+
+exports.notifyUserUpdate = functions.database.ref('/users/{id}')
+    .onUpdate((snapshot) => {
+        const newData = snapshot.after.val();
+        if (newData === null) return;
+        const msg = {
+            data: {
+                type: 'userUpdate',
+                value: JSON.stringify(newData)
+            },
+            topic: `user${newData.id}`
+        };
+        return admin.messaging().send(msg).then((response) => {
+            console.log(`Successfully sent /users/${newData.id} update:`, response);
+            return 0;
+        }).catch((error) => {
+            console.log(`Error sending /users/${newData.id} update:`, error);
+            throw error;
+        });
+    });
+
+exports.notifyCourseUpdate = functions.database.ref('/courses/{id}')
+    .onUpdate((snapshot) => {
+        const newData = snapshot.after.val();
+        if (newData === null) return;
+        const msg = {
+            data: {
+                type: 'courseUpdate',
+                value: JSON.stringify(newData)
+            },
+            topic: `courseUpdates`
+        };
+        return admin.messaging().send(msg).then((response) => {
+            console.log(`Successfully sent /courses/${newData.id} update:`, response);
+            return 0;
+        }).catch((error) => {
+            console.log(`Error sending /courses/${newData.id} update:`, error);
+            throw error;
+        });
+    });
+
+exports.notifyMaterialUpdate = functions.database.ref('/materials/{id}')
+    .onUpdate((snapshot) => {
+        const newData = snapshot.after.val();
+        if (newData === null) return;
+        const msg = {
+            data: {
+                type: 'materialUpdate',
+                value: JSON.stringify(newData)
+            },
+            topic: `course${newData.courseId}`
+        };
+        return admin.messaging().send(msg).then((response) => {
+            console.log(`Successfully sent /materials/${newData.id} update:`, response);
+            return 0;
+        }).catch((error) => {
+            console.log(`Error sending /materials/${newData.id} update:`, error);
+            throw error;
+        });
+    });
